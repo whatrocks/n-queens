994ff1ecd0bd69f768128a1ee0e78b613c0bf486
whatrocks <charles.harrington@gmail.com>
2015-10-12 15:28:40 -0700

    implemented basic solution for NRooks; passed first test

--- src/solvers.js
+++ src/solvers.js
@@ -16,10 +16,30 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = new Board({n: n});
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  var counter = 0;
+  //loop through rows
+  for ( var row = 0; row < n; row++) {
+    for (var col = 0; col < n; col++ ) {
+      solution.togglePiece(row, col); 
+      if ( !solution.hasRowConflictAt(row) && !solution.hasColConflictAt(col) ) {
+          counter++;
+          break;
+      } else {
+          solution.togglePiece(row, col);
+      }
+    }
+  }
+
+  if ( counter === n) {
+    console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  //  solution.print();
+    return solution.rows();
+  } else {
+    console.log("Didn't work");
+  }
+
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
